74dcc19d03fce0cd770b9c444d6cae040ca19933
vladimirovicp <vladimirovicp70@gmail.com>
2022-10-02 18:33:24 +0400

    feat: start webpack

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # layout-lymphasleep
+
+[https://www.youtube.com/watch?v=R9DTbDA_ZVE](https://www.youtube.com/watch?v=R9DTbDA_ZVE)
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+const isProd = !isDev;
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './js/main.js',
+    output: {
+        filename: `./js/${filename('js')}`,
+        path: path.resolve(__dirname, 'app'),
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template:  path.resolve(__dirname, 'src/index.html'),
+            filename: "index.html",
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
