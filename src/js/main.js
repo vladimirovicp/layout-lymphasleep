b934873933291145b5997b9058f14be3b107e174
vladimirovicp <vladimirovicp70@gmail.com>
2022-10-13 00:15:10 +0400

    add: planets script

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,49 @@
 // import '../style.css';
 
+// import CircleType from `circletype`;
+
 import '../scss/main.scss';
 import '../index.html';
 
 import '../js/setting';
+
+
+
+const hero = document.querySelector('.hero');
+if (hero){
+    const planetaUp = hero.querySelectorAll('.planet__anima .planet__text-up');
+    if( planetaUp.length > 0){
+        planetaUp.forEach( planeta => {
+            planeta.innerHTML = planeta.textContent.replace(/\S/g, "<span>$&</span>");
+            const el = planeta.querySelectorAll('span');
+            for(let i=0; i<el.length;i++){
+                el[i].style.transform = "rotate("+(i - (el.length -1)/2) * 18+"deg)";
+            }
+        })
+    }
+
+    const planetaDown = hero.querySelectorAll('.planet__anima .planeta__text-down');
+    if( planetaDown.length > 0){
+        planetaDown.forEach( planeta => {
+            planeta.innerHTML = planeta.textContent.replace(/\S/g, "<span>$&</span>");
+            const el = planeta.querySelectorAll('span');
+            for(let i=0; i<el.length;i++){
+                // el[i].style.transform = "rotate("+(i - (el.length -1)/2) * 27+"deg)";
+
+                el[el.length - i - 1].style.transform = "rotate("+(i - (el.length -1)/2) * 27+"deg)";
+
+                // el[i].style.transform = "rotate("+( (el.length -1)/2) + i   * 36 + "deg)";
+            }
+        })
+    };
+
+}
+
+
+// const text = document.querySelector('.planet__mercury_text-up');
+// //разбивка текста на буквы
+// text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+// let element = document.querySelectorAll('span');
+// for(let i=0; i<element.length;i++){
+//     element[i].style.transform = "rotate("+(i - (element.length -1)/2) * 18+"deg)";
+// }
