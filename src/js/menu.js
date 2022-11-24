ab1ac7d1327f16d57815a861888786ce67d19db3
vladimirovicp <vladimirovicp70@gmail.com>
2022-11-24 17:01:16 +0400

    add: menu adaptive

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,20 @@
-const icons = document.querySelectorAll('.menu__icon');
-icons.forEach (icon => {
-    icon.addEventListener('click', (event) => {
-        icon.classList.toggle("open");
+const header = document.querySelector('.header');
+const menu = header.querySelector('.menu');
+const iconMenu = menu.querySelector('.menu__icon');
+const menuWrapper = menu.querySelector('.menu__wrapper');
+
+
+
+if(iconMenu){
+    iconMenu.addEventListener('click', (event) => {
+        iconMenu.classList.toggle("open");
+        if(menuWrapper){
+            menuWrapper.classList.toggle("_active");
+        }
     });
-});
+
+    // _active
+    // menu__wrapper
+
+}
 
