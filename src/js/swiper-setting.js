01acb9b9657cffe19bd81a0efc036c003cc0bfc8
vladimirovicp <vladimirovicp70@gmail.com>
2024-09-27 15:28:35 +0400

    feat: запустил

--- src/js/swiper-setting.js
+++ src/js/swiper-setting.js
@@ -1,5 +1,5 @@
-import Swiper from 'swiper/swiper-bundle.esm.js';
-
+//import Swiper from 'swiper/swiper-bundle.esm.js';
+import Swiper from 'swiper';
 // https://codepen.io/emranweb/pen/wVaWNN  - анимация в слайдере
 
 const commandDirectionsSwiper = new Swiper('.command-directions__swiper', {
